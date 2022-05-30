fe6a757579b17568608fe4041a3729d4e89eca02
Pablo Sánchez <pablosanchez@Pablos-MacBook-Air.local>
2022-05-30 12:43:39 +0100

    create form to create new products

--- inventory_manager/inventory/views.py
+++ inventory_manager/inventory/views.py
@@ -22,11 +22,29 @@ def create_product(request):
 
     if request.method == "POST":
         print('POST FORM')
+        
+        form_data = {
+            'name': request.POST['name'],
+            'sku': request.POST['sku'],
+            'status': request.POST['status'],
+            'stock': request.POST['stock'],
+            'price': request.POST['price'],
+            'description': request.POST['description'],
+        }
+        
+        new_product_form = NewProductForm(form_data)
+
+        if new_product_form.is_valid():
+            print('form is valid')
+            product = new_product_form.save(commit=False)
+            product.save()
+            print('saved product')
+
     else:
         new_product_form = NewProductForm()
 
-        context = {
-            'new_product_form': new_product_form,
-        }
+    context = {
+        'new_product_form': new_product_form,
+    }
 
     return render(request, 'inventory/create_product.html', context)
